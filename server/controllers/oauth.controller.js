3b27d0cc72b0914f3401aa9acbfa4ce75e6dce72
Cxvarisorka <lcxvaradze2006@gmail.com>
2025-11-30 20:41:53 +0400

    Problem fixed

--- server/controllers/oauth.controller.js
+++ server/controllers/oauth.controller.js
@@ -7,10 +7,14 @@ const GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth";
 const GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token";
 const GOOGLE_USERINFO_URL = "https://www.googleapis.com/oauth2/v3/userinfo";
 
+const FACEBOOK_CLIENT_ID = process.env.FACEBOOK_CLIENT_ID;
+const FACEBOOK_CLIENT_SECRET = process.env.FACEBOOK_CLIENT_SECRET;
+const REDIRECT_URI = "http://localhost:3000/api/oauth/facebook/callback";
+
 const getGoogleAuthUrl = (req, res) => {
     const params = new URLSearchParams({
-        client_id: process.env.GOOGLE_CLIENT_ID,
-        redirect_uri: process.env.GOOGLE_REDIRECT_URI,
+        client_id: process.env.MAIN_GOOGLE_ID,
+        redirect_uri: process.env.MAIN_GOOGLE_REDIRECT,
         response_type: "code",
         scope: "openid email profile",
         access_type: "offline",
@@ -19,7 +23,7 @@ const getGoogleAuthUrl = (req, res) => {
 
 
     res.redirect(`${GOOGLE_AUTH_URL}?${params.toString()}`);
-}
+};
 
 const googleCallback = async (req, res, next) => {
     try {
@@ -27,9 +31,9 @@ const googleCallback = async (req, res, next) => {
 
         const tokenResponse = await axios.post(GOOGLE_TOKEN_URL, {
             code,
-            client_id: process.env.GOOGLE_CLIENT_ID,
-            client_secret: process.env.GOOGLE_CLIENT_SECRET,
-            redirect_uri: process.env.GOOGLE_REDIRECT_URI,
+            client_id: process.env.MAIN_GOOGLE_ID,
+            client_secret: process.env.MAIN_GOOGLE_SECRET,
+            redirect_uri: process.env.MAIN_GOOGLE_REDIRECT,
             grant_type: 'authorization_code'
         });
 
@@ -67,7 +71,7 @@ const googleCallback = async (req, res, next) => {
         const cookieOptions = {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'prod',
-            sameSite: 'Lax',
+            sameSite: 'None',
             maxAge: process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
         };
 
@@ -81,4 +85,42 @@ const googleCallback = async (req, res, next) => {
     }
 };
 
-module.exports = {googleCallback, getGoogleAuthUrl};
+const getFacebookAuthUrl = (req, res) => {
+    const params = new URLSearchParams({
+        client_id: FACEBOOK_CLIENT_ID,
+        redirect_uri: REDIRECT_URI,
+        scope: "public_profile",
+    });
+
+    res.redirect(`https://www.facebook.com/v17.0/dialog/oauth?${params.toString()}`);
+};
+
+const facebookCallback = async (req, res) => {
+    try {
+        const { code } = req.query;
+
+        const tokenResponse = await axios.get("https://graph.facebook.com/v17.0/oauth/access_token", {
+            params: {
+                client_id: FACEBOOK_CLIENT_ID,
+                client_secret: FACEBOOK_CLIENT_SECRET,
+                redirect_uri: REDIRECT_URI,
+                code,
+            },
+        });
+
+        const { access_token } = tokenResponse.data;
+
+        const userInfo = await axios.get("https://graph.facebook.com/me", {
+            params: {
+                fields: "id,name,email,picture",
+                access_token
+            }
+        });
+
+        console.log(userInfo.data);
+    } catch(err) {
+        console.log(err);
+    }
+};
+
+module.exports = {googleCallback, getGoogleAuthUrl, getFacebookAuthUrl, facebookCallback};
